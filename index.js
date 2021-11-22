460a63c9ebb6238b219a95ba7bc9bae633e2d5d8
mikeymizell <61428335+mikeymizell@users.noreply.github.com>
2021-11-21 16:57:33 -0800

    added questions and ability to read data

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const personalQuestions = ["What is your name?", "What is your GitHub username?",
+    "What is your email?"];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const projectQuestions = ["What is the name of this project?", "What languages did you use?",
+    "What is the GitHub repo link?", "What is the GitHub deployed page link?", "Brief description: "];
 
-// TODO: Create a function to initialize app
-function init() {}
+const promptUser = () => {
+    console.log(`====== README Personal Questions ======`);
 
-// Function call to initialize app
-init();
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: personalQuestions[0]
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: personalQuestions[1]
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: personalQuestions[2]
+        }
+    ]);
+};
+
+const promptQuestions = readmeDATA => {
+    console.log(`====== README Project Questions ======`);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: projectQuestions[0]
+        },
+        {
+            type: 'checkbox',
+            name: 'lang',
+            message: projectQuestions[1],
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'repoLink',
+            message: projectQuestions[2]
+        },
+        {
+            type: 'input',
+            name: 'pageLink',
+            message: projectQuestions[3]
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: projectQuestions[4]
+        }
+    ])
+    .then(projectData => {
+        readmeDATA.project = projectData;
+        return readmeDATA;
+    });
+}
+
+promptUser()
+    .then(promptQuestions)
+    .then(readmeDATA => {
+        console.log(readmeDATA);
+        
+        const readmeTXT = generateMarkdown(readmeDATA);
+    });
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,10 +11,9 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+
+module.exports = templateData => {
+  return `# ${templateData.name}
 
 `;
-}
-
-module.exports = generateMarkdown;
+};
